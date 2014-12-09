787092415d40f0fa8c2a2948d276a6f1a0ce1512
Artur F <schirmcharmemelone@gmail.com>
2014-12-09 12:48:39 +0100

    Delete of obsolete files

--- src_gui/FileReader.cpp
+++ src_gui/FileReader.cpp
@@ -5,14 +5,18 @@ void FileReader::loadFile(QString path){
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 
-
+    //open a file
+    qDebug() << "trying to open: \n" << path << "\n";
     file = new QFile(path);
+    file->open(QIODevice::ReadOnly);
 
-    //do something here
+    //read each line of that file and save it as a List
+    //(line1)->(line2)->(line3)-> etc...
+    while (file->bytesAvailable()) {
 
-
-
-
+        QByteArray line = file->readLine();
+        LineList << line;
+    }
 
     file->close();
 }
@@ -23,6 +27,16 @@ void FileReader::parseFile(){
 
 }
 
+void FileReader::printFile(){
+    if (LineList.isEmpty()) {
+        qDebug() << "LineList.isEmpty()! \nthe parser didnt read the file correctly!";
+    }else{
+        qDebug()<< "LineList is not Empty!:\n";
+        foreach (QString line, LineList) {
+            qDebug()<< "\n" << line;
+        }
+    }
+}
 
 void FileReader::writeFile(){
 
