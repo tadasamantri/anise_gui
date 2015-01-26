63034630eb748bfa7bab2f20b263335ae27de334
Tobias Lippert <tobi_lippert@hotmail.de>
2015-01-26 11:31:46 +0100

    simplified create methods

--- src_gui/nodefactory.cpp
+++ src_gui/nodefactory.cpp
@@ -18,8 +18,7 @@ Node NodeFactory::createNode() {
 }
 
 Node NodeFactory::createNode(QString _class) {
-    Node node(NodeCatalog::instance()->getNodeOfType(_class));
-    node.setType(_class);
+    Node node = NodeCatalog::instance()->getNodeOfType(_class);
     return node;
 }
 
@@ -37,7 +36,7 @@ Node NodeFactory::createNode(QString _class, QString name, QVariantMap params) {
         //qDebug() << "params.find(key).value() " << params.find(key).value();
 
 
-        node.addParam(key, params.find(key).value());
+        node.addParam(key, params[key]);
     }
     return node;
 }
