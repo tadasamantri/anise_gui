52d4a554ab741195a029be180b7c7e8590b0fa48
Tobias Lippert <tobi_lippert@hotmail.de>
2015-01-31 21:34:42 +0100

    removed copies of images, added tooltip to drawn nodes

--- src_gui/singletonrender.cpp
+++ src_gui/singletonrender.cpp
@@ -1,4 +1,4 @@
-#include <stddef.h> // defines NULL
+#include <stddef.h>  // defines NULL
 #include "singletonrender.h"
 #include "testdrawobject.h"
 #include "nodetypelabel.h"
@@ -46,11 +46,11 @@ void SingletonRender::drawLine(double start_x, double start_y, double end_x,
 void SingletonRender::paintEvent(QPaintEvent *event) {
     qDebug() << "draw event";
     /*
-   foreach (QLine* d, this->allDrawnLines) {
-       QPainter painter(this->ui->meshField);
-       painter.setPen(QPen(Qt::black, 12, Qt::DashDotLine, Qt::RoundCap));
-       painter.drawLine(d);
-   }*/
+ foreach (QLine* d, this->allDrawnLines) {
+     QPainter painter(this->ui->meshField);
+     painter.setPen(QPen(Qt::black, 12, Qt::DashDotLine, Qt::RoundCap));
+     painter.drawLine(d);
+ }*/
 }
 
 // loads all images in the ../DataIimages folder.
@@ -102,57 +102,60 @@ bool SingletonRender::loadImages() {
 void SingletonRender::setUi(Ui::MainWindow *ui) { this->ui = ui; }
 
 void SingletonRender::renderNode(Node *nodeToRender, int nodeID) {
-
     if (!allDrawnNodes.contains(nodeID)) {
         // create a Drawobject
-        DrawObject *NodeDrawObject = new DrawObject(nodeID, QPoint (int (nodeToRender->position_x), int (nodeToRender->position_y)), this->ui->meshField);
+        DrawObject *NodeDrawObject = new DrawObject(
+                    nodeID,
+                    QPoint(int(nodeToRender->position_x), int(nodeToRender->position_y)),
+                    this->ui->meshField);
 
+        /* if (allImages.contains("background.png")) {
+         // Draw the bg
+         NodeDrawObject->addPicture(allImages["background.png"], QPoint(10,10));
+             qDebug() << "background.png loaded";
 
-       /* if (allImages.contains("background.png")) {
-            // Draw the bg
-            NodeDrawObject->addPicture(allImages["background.png"], QPoint(10,10));
-                qDebug() << "background.png loaded";
-
-        } else {
-            qDebug() << "background.png did not load correctly!";
-        }*/
+     } else {
+         qDebug() << "background.png did not load correctly!";
+     }*/
 
         if (allImages.contains("body.png")) {
-                    // Draw the body
-                    NodeDrawObject->addPicture(allImages["body.png"], QPoint(15,0), 60, 80);
-                        qDebug() << "body.png loaded";
+            // Draw the body
+            NodeDrawObject->addPicture(allImages["body.png"], QPoint(0, 0), 60, 80);
+            NodeDrawObject->setMask(allImages["body.png"]->mask());
+            qDebug() << "body.png loaded";
 
-                } else {
-                    qDebug() << "body.png did not load correctly!";
-                }
+        } else {
+            qDebug() << "body.png did not load correctly!";
+        }
 
         if (allImages.contains("gate.png")) {
-                    // Draw the body
+            // Draw the body
             /*
-                    NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,10), 15, 15);
-                    NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,32), 15, 15);
-                    NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,55), 15, 15);
-                    NodeDrawObject->addPicture(allImages["gate.png"], QPoint(75,30), 15, 15);
-                        qDebug() << "gate.png loaded";*/
+              NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,10),
+         15, 15);
+              NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,32),
+         15, 15);
+              NodeDrawObject->addPicture(allImages["gate.png"], QPoint(0,55),
+         15, 15);
+              NodeDrawObject->addPicture(allImages["gate.png"], QPoint(75,30),
+         15, 15);
+                  qDebug() << "gate.png loaded";*/
 
-                } else {
-                    qDebug() << "body.png did not load correctly!";
-                }
-
-
-
-       /* if (allImages.contains("monster.png")) {
-            // Draw the mnster
-
-            NodeDrawObject->addPicture(allImages["monster.png"], QPoint (10,0));
-            // Set the geometry to the right size
-            NodeDrawObject->setGeometry(0, 0, 50, 50);
         } else {
-            qDebug() << "monster.png did not load correctly!";
-        }*/
+            qDebug() << "body.png did not load correctly!";
+        }
 
+        /* if (allImages.contains("monster.png")) {
+         // Draw the mnster
 
+         NodeDrawObject->addPicture(allImages["monster.png"], QPoint (10,0));
+         // Set the geometry to the right size
+         NodeDrawObject->setGeometry(0, 0, 50, 50);
+     } else {
+         qDebug() << "monster.png did not load correctly!";
+     }*/
 
+        NodeDrawObject->setToolTip(nodeToRender->getDescription());
         allDrawnNodes.insert(nodeID, NodeDrawObject);
     }
     // TODO should use layouts instead of hardcoded position!
@@ -180,12 +183,12 @@ void SingletonRender::clearMeshField() { clearAll(ui->meshField); }
 
 void SingletonRender::renderNodeType(Node *nodeToRender, QWidget *parent,
                                      int position) {
-
     // TODO code dublication in renderNode and renderNodeType!
     NodeTypeLabel *NodeDrawObject = new NodeTypeLabel(parent);
 
     // Zeichne den hintergrund:
     NodeDrawObject->setPixmap(*allImages["background.png"]);
+    NodeDrawObject->setMask(allImages["background.png"]->mask());
     NodeDrawObject->setGeometry(0, 0, 50, 50);
     // Generate a Tooltip
     QString type = nodeToRender->getType();
@@ -194,8 +197,9 @@ void SingletonRender::renderNodeType(Node *nodeToRender, QWidget *parent,
     toolTip = "NodeClass = " + nodeToRender->getType() + "\nInputs: " +
             QString::number(nodeToRender->getInputGates()->size()) +
             "\nOutputs: " +
-            QString::number(nodeToRender->getOutputGates()->size()) + "\n" +
-            nodeToRender->getDescription();
+            QString::number(nodeToRender->getOutputGates()->size());
+    if (nodeToRender->getDescription() != "")
+        toolTip += "\n" + nodeToRender->getDescription();
     NodeDrawObject->setToolTip(toolTip);
     // TODO should use layouts instead of hardcoded position!
     NodeDrawObject->move(5, 5 + position * 60);
@@ -204,7 +208,6 @@ void SingletonRender::renderNodeType(Node *nodeToRender, QWidget *parent,
 }
 
 void SingletonRender::renderCatalogContent(QVector<Node> NodeVektor) {
-
     QWidget *CatalogParent = ui->nodeCatalogContent;
     int position = 0;
     // TODO scroll weite sollte nicht hard coded sein
@@ -216,37 +219,26 @@ void SingletonRender::renderCatalogContent(QVector<Node> NodeVektor) {
 }
 
 void SingletonRender::clearAll(QWidget *parent) {
-
     //
-    while ( QWidget* w = parent->findChild<DrawObject*>() ){
+    while (QWidget *w = parent->findChild<DrawObject *>()) {
         w->deleteLater();
         delete w;
-}
-
+    }
 
     // TODO NOT COOL THIS SOLUTION!
     if (parent == ui->meshField) {
-
         this->allDrawnNodes = QMap<int, DrawObject *>();
         this->allDrawnLines = QMap<int, QLine *>();
     }
 }
 
+bool SingletonRender::deleteMeshDrawing(int objectID) {
+    DrawObject *childToDelete = allDrawnNodes.value(objectID);
 
-bool SingletonRender::deleteMeshDrawing(int objectID){
+    allDrawnNodes.remove(objectID);
+    childToDelete->deleteLater();
 
-
-
-   DrawObject *childToDelete = allDrawnNodes.value(objectID);
-
-
-   allDrawnNodes.remove(objectID);
-   childToDelete->deleteLater();
-
-
-
-   return !allDrawnNodes.contains(objectID);
-
+    return !allDrawnNodes.contains(objectID);
 }
 
 /**
@@ -257,36 +249,26 @@ bool SingletonRender::deleteMeshDrawing(int objectID){
  */
 
 void SingletonRender::showTestWidget() {
-
-    DrawObject *dummy =
-            new DrawObject(100, QPoint(20, 20), ui->meshField);
+    DrawObject *dummy = new DrawObject(100, QPoint(20, 20), ui->meshField);
 
     qDebug() << "dummy läuft";
 
-    dummy->addPicture(this->allImages.value("background.png"), QPoint(20, 20), 10, 10);
-    dummy->addPicture(this->allImages.value("background.png"), QPoint(40, 40), 10, 10);
+    dummy->addPicture(this->allImages.value("background.png"), QPoint(20, 20), 10,
+                      10);
+    dummy->addPicture(this->allImages.value("background.png"), QPoint(40, 40), 10,
+                      10);
     qDebug() << "dummy läuft";
 }
 
-
-QVector<int> *SingletonRender::getChildrenIDs(){
-
-
-    QVector<int>* ids = new QVector<int>();
+QVector<int> *SingletonRender::getChildrenIDs() {
+    QVector<int> *ids = new QVector<int>();
     QObjectList children = this->ui->meshField->children();
 
+    foreach (QObject *child, children) {
+        DrawObject *node = dynamic_cast<DrawObject *>(child);
 
-    foreach(QObject *child , children){
-
-
-        DrawObject* node  = dynamic_cast<DrawObject*> (child);
-
-        if(node != NULL)
-            ids->push_back(node->nodeID);
+        if (node != NULL) ids->push_back(node->nodeID);
     }
 
     return ids;
-
 }
-
-
